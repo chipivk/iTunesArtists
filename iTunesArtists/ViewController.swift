b42c1a87083c8841daf8227b596c04a287753b80
Carlos López <carlosl.fernandez@colaboradoresunidadeditorial.es>
2017-11-23 07:56:27 +0100

    Initial commit

---
+++ iTunesArtists/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iTunesArtists
+//
+//  Created by Carlos López on 23/11/17.
+//  Copyright © 2017 Carlos López. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
